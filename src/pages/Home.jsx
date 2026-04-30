3a293f9a670a58f6a5efc592a428fe5b84ab37cb
noahfarinha <noe-rendeiro@hotmail.com>
2026-04-30 18:06:55 +0100

    product card file created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
+import ProductCard from "../components/ProductCard";
 import { getProducts } from "../data/products";
 import {Link} from "react-router-dom";
-
 export default function Home(){
     const products = getProducts();
     return (
@@ -13,19 +13,9 @@ export default function Home(){
 
         <div className="container">
             <h2 className="page-title">Product List</h2>
-            <div className="products-grid">
+            <div className="product-grid">
                 {products.map((product) => (
-                    <div className="product-card" key={product.id}>
-                      <img src={product.image} className="product-card-image" />  
-                      <div className="product-card-content">
-                        <h3 className="product-card-name">{product.name}</h3>
-                        <p className="product-card-price">£{product.price}</p>
-                        <div className="product-card-actions">
-                            <Link className="btn btn-secondary">View Details</Link>
-                            <button className="btn btn-primary">Add to Cart</button>
-                            </div>
-                        </div>
-                    </div> 
+                    <ProductCard product={product} key={product.id}/>
                 ))}
 
             </div>
