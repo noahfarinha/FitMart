d5d93c4cb97d7192b684a2faf6de0eab6562c085
noahfarinha <noe-rendeiro@hotmail.com>
2026-04-30 17:54:51 +0100

    buttons added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { getProducts } from "../data/products";
+import {Link} from "react-router-dom";
 
 export default function Home(){
     const products = getProducts();
@@ -17,9 +18,14 @@ export default function Home(){
                     <div className="product-card" key={product.id}>
                       <img src={product.image} className="product-card-image" />  
                       <div className="product-card-content">
-                        <h3>{product.name}</h3>
+                        <h3 className="product-card-name">{product.name}</h3>
+                        <p className="product-card-price">£{product.price}</p>
+                        <div className="product-card-actions">
+                            <Link className="btn btn-secondary">View Details</Link>
+                            <button className="btn btn-primary">Add to Cart</button>
+                            </div>
                         </div>
-                    </div>
+                    </div> 
                 ))}
 
             </div>
