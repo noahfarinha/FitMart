f456bc76e4d518f718578d783e33742a079508db
noahfarinha <noe-rendeiro@hotmail.com>
2026-04-30 19:00:13 +0100

    auth page created

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,49 @@
+import {useState} from "React";
+
 export default function Auth(){
-    return <div>Auth Page</div>;
+    const [mode, setMode] = useState("signup");
+    return (<div className="page">
+        <div className="container">
+            <div className="auth-container">
+                <h1 className="page-title">
+                    {mode === "signup" ? "Sign Up" : "Login"}
+                </h1>
+                <form className="auth-form">
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="email">
+                            Email
+                            </label>
+                        <input className="form-input" type="email" id="email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="password">
+                            Password
+                            </label>
+                        <input className="form-input" type="passwords" id="password"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-large">{mode === "signup" ? "Sign Up" : "Login"}</button>
+                </form>
+                <div className="auth-switch">
+                   {mode === "signup" ? (
+                     <p>
+                        Already have an acc? {" "}
+                        <span className="auth-link" onClick={() => setMode ("login")}>
+                            Log In
+                            </span>
+                    </p>
+                   ) : (
+                    <p>
+                        {" "}
+                        Don't have an acc? {" "}
+                        <span className="auth-link"onClick={() => setMode ("signup")}>
+                             Sign Up
+                            </span>
+                        </p>
+                   )}
+                </div>
+            </div>
+        </div>
+    </div>
+    );
 }
