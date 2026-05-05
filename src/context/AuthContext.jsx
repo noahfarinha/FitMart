2bdb09613fb18d996d370cd334054b96fed6aef1
noahfarinha <noe-rendeiro@hotmail.com>
2026-05-05 15:38:19 +0100

    authcontext updates

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,16 +1,20 @@
 import {createContext} from "react";
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
 
 export default function AuthProvider({children}){
     const [user, setUser] = useState(null);
 
     function signUp(email, password){
+        const users =[];
 
-    }
+        const newUser = {email, password};
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+        }
 
     function login(){
 
     }
-    return <AuthContext.Provider>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider>{{signUp}}</AuthContext.Provider>;
 }
